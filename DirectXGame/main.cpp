a0122f2e4e916595ee49dd0e750b9e1b5c96dbfd
OOTSUKAMAMORU <k024g0018@g.neec.ac.jp>
2026-05-13 13:13:51 +0900

    プロジェクトファイルを追加。

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,12 @@
+#include "KamataEngine.h"
+int WINAPIWinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	KamataEngine::Initialize(L"LE3D_06_オオツカ_マモル");
+	    // メインループ
+	    while (true) {
+		if (true) {
+			break;
+		}
+	}
+	KamataEngine::Finalize();
+	return 0;
+}
